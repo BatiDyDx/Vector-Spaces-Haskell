3e7f75cb910265ee5c1fdb6532a070023567bcfc
JuanFigueredo-c <75329583+JuanFigueredo-c@users.noreply.github.com>
2023-05-03 19:30:50 -0300

    Add files via upload

--- main.hs
+++ main.hs
@@ -1,14 +1,16 @@
 module VecSpace where
 
+type Axis = Int
+
 data NdTree p = Node (NdTree p) -- subarbol izquierdo
                 p -- punto
                 (NdTree p) -- sub ́arbol derecho
-                Int -- eje
+                Axis -- eje
                 | Empty
-                deriving (Eq, Ord, Show)
+                deriving (Show)
 
 class Eq p => Punto p where
-  dimension :: p -> Int -- devuelve el n ́umero de coordenadas de un punto
+  dimension :: p -> Int -- devuelve el numero de coordenadas de un punto
   coord :: Int -> p -> Double -- devuelve la coordenada k-esima de un punto (comenzando de 0)
   
   dist :: p -> p -> Double -- calcula la distancia entre dos puntos
@@ -18,26 +20,24 @@ class Eq p => Punto p where
 newtype Punto2d = P2d (Double, Double) deriving (Show, Eq)
 newtype Punto3d = P3d (Double, Double, Double) deriving (Show, Eq)
 
+-- instance Punto a => Eq a where
+--   p == q = (dist p q) == 0
+
 instance Punto Punto2d where
-  dimension :: Punto2d -> Int
   dimension _ = 2
 
-  coord :: Int -> Punto2d -> Double
   coord 0 (P2d (x,_)) = x
   coord 1 (P2d (_,y)) = y
   coord _ _ = error "Coordenada invalida"
 
 instance Punto Punto3d where
-  dimension :: Punto3d -> Int
   dimension _ = 3
 
-  coord :: Int -> Punto3d -> Double
   coord 0 (P3d (x,_,_)) = x
   coord 1 (P3d (_,y,_)) = y
   coord 2 (P3d (_,_,z)) = z
   coord _ _ = error "Coordenada invalida"
 
-type Axis = Int
 
 qsort :: Ord b => (a -> b) -> [a] -> [a]
 qsort _ [] = []
@@ -81,6 +81,69 @@ insertar x t@(Node l y r axis)
             | coord axis x > coord axis y = 
               case r of
                 Empty -> (Node l y (Node Empty x Empty (nextAxis axis (dimension x))) axis) 
-                _     -> (Node l x (insertar x r) axis) 
+                _     -> (Node l x (insertar x r) axis)
 
+minimumNd :: Punto p => NdTree p -> Axis -> p
+minimumNd (Node Empty y Empty axis) _ = y
+minimumNd (Node l y r axis) eje | axis == eje = minimumNd l eje
+                                | otherwise = let (x,y) = (minimumNd l eje, minimumNd r eje)
+                                              in if (coord eje x) <= (coord eje y) then x else y
 
+maximumNd :: Punto p => NdTree p -> Axis -> p
+maximumNd (Node Empty y Empty axis) _ = y
+maximumNd (Node l y r axis) eje | axis == eje = maximumNd r eje
+                                | otherwise = let (x,y) = (maximumNd l eje, maximumNd r eje)
+                                              in if (coord eje x) >= (coord eje y) then x else y
+
+eliminar :: Punto p => p -> NdTree p -> NdTree p
+eliminar _ Empty = Empty
+eliminar x t@(Node Empty y Empty axis) | x == y = Empty
+                                       | otherwise = t
+eliminar x t@(Node l y r axis)
+        | x /= y = if (coord axis x) <= (coord axis y) then (Node (eliminar x l) y r axis)
+                   else (Node l y (eliminar x r) axis) 
+        | otherwise = case r of
+                        Empty -> let max = maximumNd l axis
+                                     l' = eliminar max l
+                                 in (Node l' max r axis)
+                        _ ->     let min = minimumNd r axis
+                                     r' = eliminar min r
+                                 in (Node l min r' axis)
+              
+type Rect = (Punto2d, Punto2d) -- Esquina inferior izquierda y esquina superior derecha
+
+inRegion :: Punto2d -> Rect -> Bool
+inRegion (P2d (x,y)) ((P2d (x0, y0)), (P2d (x1, y1))) = x0 <= x && x <= x1
+                                      && y0 <= y && y <= y1
+
+fueraIzq :: Punto2d -> Rect -> Bool
+fueraIzq (P2d (x,_)) ((P2d (x0,_)), _) = x0 > x
+
+fueraDer :: Punto2d -> Rect -> Bool
+fueraDer (P2d (x,_)) (_, (P2d (x1,_))) = x1 < x
+
+fueraArr :: Punto2d -> Rect -> Bool
+fueraArr (P2d (_,y)) (_, (P2d (_, y1))) = y1 < y
+
+fueraAbj :: Punto2d -> Rect -> Bool
+fueraAbj (P2d (_,y)) ((P2d (_,y0)), _) = y0 > y
+
+branchLeft :: Punto2d -> Rect -> Axis -> Bool
+branchLeft p rect 0 = not (fueraIzq p rect)
+branchLeft p rect 1 = not (fueraAbj p rect)
+
+branchRight :: Punto2d -> Rect -> Axis -> Bool
+branchRight p rect 0 = not (fueraDer p rect)
+branchRight p rect 1 = not (fueraArr p rect)
+
+ortogonalSearch :: NdTree Punto2d -> Rect -> [Punto2d]
+ortogonalSearch Empty _ = []
+ortogonalSearch (Node l p r axis) rect =
+        let ps = if inRegion p rect then [p] else []
+            ls = if (branchLeft p rect axis) --- NOMBRE AUXILIAR 
+                 then ortogonalSearch l rect
+                 else []
+            rs = if (branchRight p rect axis) --- NOMBRE AUXILIAR 
+                 then ortogonalSearch r rect
+                 else []
+        in ps ++ rs ++ ls
